1f69208662416f170ff76dcbe8274c5617e677af
kevinkennedy <@kennedy.kev@northeastern.edu>
2024-04-15 14:12:19 -0400

    push

--- Kanbas/Database/index.js
+++ Kanbas/Database/index.js
@@ -2,4 +2,4 @@ import courses from "./courses.js";
 import modules from "./modules.js";
 import assignments from "./assignments.js";
 import quizzes from "./quizzes.js";
-export default { courses, modules, assignments, quizzes };
+export default { courses, modules, assignments, quizzes }; 
